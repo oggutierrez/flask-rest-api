4f8ace0405b545a482ba49cf37c1b20aeb9d4a4e
Oscar <oscar.gutierrez@wize.pe>
2018-10-19 10:23:29 -0500

    First commit, added REST API to access items, stores and user authentication.

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify
+from flask_jwt import JWT
+from flask_restful import Api
+
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from resources.user import UserRegister
+from security import authenticate, identity as identity_function
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'oscar'
+api = Api(app)
+
+
+@app.before_first_request
+def creat_tables():
+    db.create_all()
+
+
+# change default authentication URL
+# app.config['JWT_AUTH_URL_RULE'] = '/login'
+# config JWT to expire within half an hour
+# app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+# config JWT auth key name to be 'email' instead of default 'username'
+# app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
+jwt = JWT(app, authenticate, identity_function)  # /auth
+
+
+@jwt.auth_response_handler
+def customized_response_handler(access_token, identity):
+    return jsonify({
+        'access_token': access_token.decode('utf-8'),
+        'user_id': identity.id
+    })
+
+
+@jwt.jwt_error_handler
+def customized_error_handler(error):
+    return jsonify({
+        'message': error.description,
+        'code': error.status_code
+    }), error.status_code
+
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    from db import db
+
+    db.init_app(app)
+    app.run(port=5000, debug=True)
