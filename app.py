9474fe2e9164049bf6780cd2a88099d7cb9de1fb
Oscar <oscar.gutierrez@wize.pe>
2018-10-22 15:21:49 -0500

    Finish course

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
 
 from flask import Flask, jsonify
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
+from blacklist import BLACKLIST
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from resources.user import UserRegister
-from security import authenticate, identity as identity_function
+from resources.user import UserRegister, User, UserLogin, TokenRefresh, UserLogout
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'oscar'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.secret_key = 'oscar'  # app.config['JWT_SECRET_KEY']
 api = Api(app)
+
 
 # change default authentication URL
 # app.config['JWT_AUTH_URL_RULE'] = '/login'
@@ -22,31 +26,91 @@ api = Api(app)
 # config JWT auth key name to be 'email' instead of default 'username'
 # app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
 
-jwt = JWT(app, authenticate, identity_function)  # /auth
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
-@jwt.auth_response_handler
-def customized_response_handler(access_token, identity):
+jwt = JWTManager(app)  # # not creating /auth
+
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:  # Instead of hard-coding, you shoudl read from a config file or a database
+        return {'is_admin': True}
+    return {'is_admin': False}
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
+
+
+@jwt.expired_token_loader
+def expired_token_callback():
     return jsonify({
-        'access_token': access_token.decode('utf-8'),
-        'user_id': identity.id
-    })
+        'description': 'The token has expired',
+        'error': 'token_expired'
+    }), 401
 
 
-@jwt.jwt_error_handler
-def customized_error_handler(error):
+@jwt.invalid_token_loader  # Wrong token
+def invalid_token_callback(error):
     return jsonify({
-        'message': error.description,
-        'code': error.status_code
-    }), error.status_code
+        'description': 'Signature verification failed',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh',
+        'error': 'fresh_token_required'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoke_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked',
+        'error': 'token_revoked'
+    }), 401
+
+
+# @jwt.auth_response_handler
+# def customized_response_handler(access_token, identity):
+#     return jsonify({
+#         'access_token': access_token.decode('utf-8'),
+#         'user_id': identity.id
+#     })
+#
+#
+# @jwt.jwt_error_handler
+# def customized_error_handler(error):
+#     return jsonify({
+#         'message': error.description,
+#         'code': error.status_code
+#     }), error.status_code
 
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
-
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     from db import db
--- resources/item.py
+++ resources/item.py
@@ -1,4 +1,10 @@
-from flask_jwt import jwt_required
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_claims,
+    jwt_optional,
+    get_jwt_identity,
+    fresh_jwt_required
+)
 from flask_restful import Resource, reqparse
 
 from models.item import ItemModel
@@ -17,11 +23,12 @@ class Item(Resource):
                         help="Every item needs a store id!"
                         )
 
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         return item.json() if item else {'message': 'Item not found'}, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': 'An item with name {} already exists'.format(name)}, 400
@@ -37,7 +44,11 @@ class Item(Resource):
 
         return item.json(), 201
 
+    @jwt_required
     def delete(self, name):
+        claims = get_jwt_claims()
+        if not claims['is_admin']:
+            return {'message': 'Admin privilege required'}, 401
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
@@ -58,5 +69,13 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
-        return {'items': [item.json() for item in ItemModel.query.all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+                   'items': [item['name'] for item in items],
+                   'message': 'More data available if you log in'
+               }, 200
